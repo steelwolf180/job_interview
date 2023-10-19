802a7d262cce8e8c6b8caa41be28d8fcf7483836
steelwolf180 <steelwolf180@gmail.com>
2023-10-19 17:51:54 +0800

    feat: Added test cases to check for using just 1 item or result should contain 1 item to fill wardrobe.

--- wardrobe/src/apps/wardrobe.py
+++ wardrobe/src/apps/wardrobe.py
@@ -1,2 +1,30 @@
 def get_wardrobe_length():
     return 150
+
+def get_wardrobe_items():
+    return {'50': 59, '75': 62, '100':90, '120': 111}
+
+def get_space_left(items, item):
+    if item == '0':
+        return 0
+    
+    elif item == '50':
+        return 0
+    
+    elif item == '75':
+        return 25
+    
+    elif item == '100':
+        return 50
+    
+    elif item == '120':
+        return 30
+    
+
+def calculate_items_used(items):
+    used = []
+    while True:
+        used.append('')    
+        if len(used) >= 1:
+            break
+    return used
--- wardrobe/src/tests/test_wardrobe.py
+++ wardrobe/src/tests/test_wardrobe.py
@@ -1,4 +1,27 @@
-from apps.wardrobe import get_wardrobe_length
+from apps.wardrobe import *
 
 def test_wardrobe_length():
     assert get_wardrobe_length() == 150
+    
+def test_wardrobe_items():
+    items = get_wardrobe_items()
+    
+    assert len(items) == 4
+    assert items['50'] == 59
+    assert items['75'] == 62
+    assert items['100'] == 90
+    assert items['120'] == 111
+
+def test_fill_with_single_item():
+    items = list(get_wardrobe_items().keys())
+    
+    assert get_space_left(items, items[0]) == 0
+    assert get_space_left(items, items[1]) == 25
+    assert get_space_left(items, items[2]) == 50
+    assert get_space_left(items, items[3]) == 30
+    assert get_space_left(items, '0') == 0
+    
+def test_calculate_space_left_with_at_least_one():
+    items = list(get_wardrobe_items().keys())
+    
+    assert len(calculate_items_used(items)) >= 1
